a034b7a931e34642be2e62edb2e1abe821ad8bfb
Sekou Dioubate <dioubatesekou@gmail.com>
2016-04-24 15:44:06 +0100

    tooltip

--- js/app.js
+++ js/app.js
@@ -7,4 +7,9 @@ $(document).ready(function () {
     $('#intro').css({
         'margin-top': -($('#intro').height() / 2)
     });
+
+
 });
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
