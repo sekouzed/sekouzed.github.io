e7a8bc58acc2818919ff5c5beeecd21924580a52
Sekou Dioubate <dioubatesekou@gmail.com>
2016-04-24 15:30:28 +0100

    particleground using jquery

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    particleground(document.getElementById('particles'), {
+$(document).ready(function () {
+    $('#particles').particleground({
         dotColor: '#5cbdaa',
         lineColor: '#5cbdaa'
     });
-    var intro = document.getElementById('intro');
-    intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
-
-
-
- // jQuery plugin example:
- $(document).ready(function() {
- $('#particles').particleground({
- dotColor: '#5cbdaa',
- lineColor: '#5cbdaa'
- });
- $('.intro').css({
- 'margin-top': -($('.intro').height() / 2)
- });
- });
+    $('#intro').css({
+        'margin-top': -($('#intro').height() / 2)
+    });
+});
